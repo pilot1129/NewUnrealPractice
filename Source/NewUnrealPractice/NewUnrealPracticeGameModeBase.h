2a8f14abb512e0e2b7b466976b08ff9c6971dc82
kim ki hwan <pilot1129@naver.com>
2021-07-15 15:28:18 +0900

    UE4 First upload
    Base Setting

---
+++ Source/NewUnrealPractice/NewUnrealPracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewUnrealPracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWUNREALPRACTICE_API ANewUnrealPracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
